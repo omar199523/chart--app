558a441e0784c9212798a9ff476f3b49a093d6f7
omar199523 <model199523@gmail.com>
2022-10-14 19:21:16 +0300

    edit front and back conect

--- client/src/componant/ChartContiner/index.js
+++ client/src/componant/ChartContiner/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import {useEffect, useState} from 'react'
+
+import { useState} from 'react'
 import TokenChart from "../TokenChart";
 import './style.css'
 const ChartContiner = ({tokenData}) => {
